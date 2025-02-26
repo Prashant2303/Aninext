5b3516406849a34cbf8e79dd216282144e13b371
Prashant2303 <prashant1997.official@gmail.com>
2025-02-26 19:46:06 +0530

    Added path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { Footer } from './components/footer'
 
 function App() {
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">    Hello world!  </h1>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Footer />
     </>
   )
 }
