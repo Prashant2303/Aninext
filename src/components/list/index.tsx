0da7b5f436a82d38cfb7cd8c7ab67b006c9faf64
Prashant2303 <prashant1997.official@gmail.com>
2025-03-02 21:12:37 +0530

    Minor fix

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,5 +1,5 @@
 import { Media } from "@/types";
-import { Card } from "../card";
+import { Card } from "@/components/card";
 
 export function List(props: { title: string, loading: boolean, media: Media[] | undefined }) {
     const { title, loading, media } = props;
