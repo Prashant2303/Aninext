f46e7fab653326607442daabf824a75fa6ab0403
Prashant2303 <prashant1997.official@gmail.com>
2025-03-02 17:40:01 +0530

    Styled heading

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router";
 
 export function Header() {
-    return <header>
+    return <header className="flex justify-center py-2">
         <Link to='/'>
-            <h1 className="text-3xl font-bold flex justify-center py-2">AniNext</h1>
+            <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-linear-[45deg,#02c1ff,#c800ff]">
+                AniNext
+            </h1>
         </Link>
     </header>
 }
